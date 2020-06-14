35c198113c33393852ade0521de30d224fafd872
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-14 02:08:40 +0200

    Used the Array.Resize method

---
+++ SimpleArrays/SimpleArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SimpleArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var groceryList = new string[4];
+            groceryList[0] = "Milk";
+            groceryList[1] = "Cheese";
+            groceryList[2] = "Eggs";
+            groceryList[3] = "Apples";
+
+            var groceryList2 = new string[2] { "Milk", "Egg" };
+            Array.Resize(ref groceryList2, 3);
+            groceryList2[2] = "Bread";
+            var groceryList3 = groceryList2;
+            groceryList3[2] = "Water";
+            Console.WriteLine(groceryList2[2]);
+        }
+    }
+}
