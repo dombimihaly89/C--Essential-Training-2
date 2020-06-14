63f8b0da8e8e849558b2d35af72ed517525255af
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-14 14:50:35 +0200

    Tried some linQ methods on an array

---
+++ EssentialTraining/EssentialTraining/SimpleLINQ.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EssentialTraining
+{
+    public class SimpleLINQ
+    {
+        public int[] ListOfNumbers { get; set; }
+
+        public SimpleLINQ()
+        {
+            ListOfNumbers = new int[5] { 1, 3, 5, 7, 9 };
+        }
+
+        public void SomeLinqMethods()
+        {
+            Console.WriteLine("Sum: " + ListOfNumbers.Sum());
+            Console.WriteLine("Average: " + ListOfNumbers.Average());
+            Console.WriteLine("Numbers bigger than or equal to 5:");
+            var result = ListOfNumbers.Where(element => element >= 5);
+            foreach(int element in result)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
--- EssentialTraining/Program/Program.cs
+++ EssentialTraining/Program/Program.cs
@@ -7,23 +7,24 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            SimpleArray simpleArray = new SimpleArray();
-            Console.WriteLine(simpleArray.ToString());
+            //SimpleArray simpleArray = new SimpleArray();
+            //Console.WriteLine(simpleArray.ToString());
 
-            for (int i = 0; i < simpleArray.Multi.GetLength(0); i++)
-            {
-                for (int j = 0; j < simpleArray.Multi.GetLength(1); j++)
-                {
-                    Console.WriteLine(simpleArray.Multi[i, j]);
-                }
-            }
+            //for (int i = 0; i < simpleArray.Multi.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < simpleArray.Multi.GetLength(1); j++)
+            //    {
+            //        Console.WriteLine(simpleArray.Multi[i, j]);
+            //    }
+            //}
 
-            Console.WriteLine(simpleArray.myList[1]);
-            simpleArray.myList.Sort();
-            Console.WriteLine(simpleArray.myList[1]);
-            Console.WriteLine(simpleArray.containsAnimal("zebra"));
+            //Console.WriteLine(simpleArray.myList[1]);
+            //simpleArray.myList.Sort();
+            //Console.WriteLine(simpleArray.myList[1]);
+            //Console.WriteLine(simpleArray.containsAnimal("zebra"));
 
-
+            var simpleLINQ = new SimpleLINQ();
+            simpleLINQ.SomeLinqMethods();
         }
     }
 }
