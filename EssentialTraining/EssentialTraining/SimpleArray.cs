04beba5806dc96da3944aeaa10bf76f125e90857
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-14 13:51:24 +0200

    Added multi dimensional array to SimpleArray class, and also iterated through in Program

--- EssentialTraining/EssentialTraining/SimpleArray.cs
+++ EssentialTraining/EssentialTraining/SimpleArray.cs
@@ -7,12 +7,18 @@ namespace EssentialTraining
     public class SimpleArray
     {
         public string[] GroceryList;
+        public int[,] Multi;
 
 
         public SimpleArray()
         {
             GroceryList = new string[4] { "Bread", "Milk", "Eggs", "Cheese" };
-
+            Multi = new int[3,4]
+            {
+                {1, 2, 3, 4 },
+                {5, 6, 7, 8 },
+                {9, 10, 11, 12 }
+            };
         }
 
         public override string ToString()
--- EssentialTraining/Program/Program.cs
+++ EssentialTraining/Program/Program.cs
@@ -9,6 +9,13 @@ namespace Program
         {
             SimpleArray simpleArray = new SimpleArray();
             Console.WriteLine(simpleArray.ToString());
+            for (int i = 0; i < simpleArray.Multi.GetLength(0); i++)
+            {
+                for (int j = 0; j < simpleArray.Multi.GetLength(1); j++)
+                {
+                    Console.WriteLine(simpleArray.Multi[i, j]);
+                }
+            }
 
         }
     }
