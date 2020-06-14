501fafedcd70219f1ee362e9c04b33e992357eb5
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-15 00:09:19 +0200

    Added two static method into the program class and used try-catch block in one of them

--- EssentialTraining/Program/Program.cs
+++ EssentialTraining/Program/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using EssentialTraining;
+using System.IO;
 
 namespace Program
 {
-    class Program
+    public class Program
     {
+        public static List<string> Words;
         static void Main(string[] args)
         {
             //SimpleArray simpleArray = new SimpleArray();
@@ -23,20 +25,61 @@ namespace Program
             //simpleArray.myList.Sort();
             //Console.WriteLine(simpleArray.myList[1]);
             //Console.WriteLine(simpleArray.containsAnimal("zebra"));
-            Console.WriteLine("LINQ: ");
-            var simpleLINQ = new SimpleLINQ();
-            simpleLINQ.SomeLinqMethods();
+            //Console.WriteLine("LINQ: ");
+            //var simpleLINQ = new SimpleLINQ();
+            //simpleLINQ.SomeLinqMethods();
 
-            Console.WriteLine("Dictionary: ");
-            var simpleDictionary = new SimpleDictionary();
-            simpleDictionary.WorkWithDictionary();
+            //Console.WriteLine("Dictionary: ");
+            //var simpleDictionary = new SimpleDictionary();
+            //simpleDictionary.WorkWithDictionary();
 
-            var ternary = ("Blue".ToLower() == "blue") ? "Yes" : "No";
-            Console.WriteLine(ternary);
+            //var ternary = ("Blue".ToLower() == "blue") ? "Yes" : "No";
+            //Console.WriteLine(ternary);
 
-            var initializedList = new List<int> { 1, 3, 4, 6, 12 };
+            //var initializedList = new List<int> { 1, 3, 4, 6, 12 };
+            /*
             // If an object implements the IEnumerable interface then it can be iterated through with foreach.
             // Collections usually are implementing the interface.
+            */
+
+            Words = new List<string>();
+            Words.Add("Bread");
+            Words.Add("Water");
+            Words.Add("Butter");
+            CrazyMathProblem();
+            ReadTextFile();
+
+        }
+
+        private static void ReadTextFile()
+        {
+            try
+            {
+                // If we use the @ sign before the quote then we don't get any problem with tha backslash character's
+                // escaping behaviour. If we didn't use then it would escape the character after that.
+                // We use the using statement here because we want to use a class that implements the IDisposable
+                // interface. When we use a class like this we don't have to deal with opening and closing the filestream.
+                using (var sr = new StreamReader(@"C:\temp\test.txt"))
+                {
+                    string contents = sr.ReadToEnd();
+                    Console.WriteLine(contents);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Couldn't read the file");
+            }
+        }
+
+        public static int CrazyMathProblem()
+        {
+            int income = 1000;
+            for (int i = 10; i > 0; i--)
+            {
+                income = (income / i);
+            }
+
+            return income;
         }
     }
 }
